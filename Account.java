6007f17b09fa6d0f07f444f88f6b0ec11ec6b083
SebastinjeyaprakashL <79531993+SebastinjeyaprakashL@users.noreply.github.com>
2022-03-22 10:50:43 +0530

    Bank Login and money transfer

---
+++ Account.java
@@ -0,0 +1,198 @@
+package myPackage;
+import java.util.*;
+
+class Bank
+{
+	
+	private long accountnumber;
+	private String name, username, password;
+	private float balance;
+	protected void setAccDetails (long accNum, String name, String username, String pwd)
+	{
+		this.accountnumber = accNum;
+		this.name = name;
+		this.username = username;
+		this.password = pwd;
+	}
+	protected long getAccNum ()
+	{
+		return this.accountnumber;
+	}
+	protected String getName ()
+	{
+		return this.name;
+	}
+	protected String getUserName ()
+	{
+		return this.username;
+	}
+	protected String getPassword ()
+	{
+		return this.password;
+	}
+	protected void setBalance (float bal)
+	{
+		this.balance = bal;
+	}
+	protected float getBalance ()
+	{
+		return this.balance;
+	}
+}
+
+class Customer1 extends Bank
+{
+	Customer1 (long myaccNum, String myName, String myUser, String myPwd)
+	{
+		setAccDetails (myaccNum, myName, myUser, myPwd);	
+		setBalance (1000);
+	}
+	boolean verifyUsername (String usrname)
+	{
+		String dbUsername = getUserName ();
+		if (dbUsername.equals (usrname))
+			return true;
+
+		else
+			return false;
+
+	}
+
+	boolean verifyPwd (String pwd)
+	{
+
+	String dbpwd = getPassword ();
+
+	if (dbpwd.equals (pwd))
+
+	return true;
+
+	else
+
+	return false;
+
+	}
+
+	boolean checkForDebit (float amount)
+	{
+		float availAmount = getBalance ();
+	if (availAmount > amount)
+	{
+		float updatedBalance = availAmount - amount;
+		setBalance (updatedBalance);
+		return true;
+	}
+	else
+		return false;
+	}
+	void updateAccBal (float balAmount)
+	{
+		float availAmount = getBalance ();
+		float updatedBalance = availAmount - balAmount;
+
+		setBalance (updatedBalance);
+	}
+	float checkbalance ()
+	{
+		float availAmount = getBalance ();
+		return availAmount;
+	}
+}
+
+public class Account
+{
+	public static final String bankName = "BANK OF ZZZ";
+public static void main (String args[])
+{
+	boolean b = true, flag = true;
+	Scanner sc = new Scanner (System.in);
+	String username, pwd;
+	int option, loginAction;
+	float debitamount;
+	Customer1 a1 = new Customer1 (1234, "Test1", "user1", "Test#@!");	// db
+	System.out.println ("Welcome to "+bankName);
+	do {
+	System.out.println("Please enter the action to proceed: \n1.Login \n2.Exit");
+	loginAction = sc.nextInt();
+	sc.nextLine();
+	if (loginAction == 1)
+	{
+	System.out.println("Please enter your Username and Password to Login \nUsername : ");
+
+	username = sc.nextLine ();
+	if (username == "0")
+	{
+		b = false;
+	}
+	else
+	{
+		boolean isUserExist = a1.verifyUsername (username);
+	if (isUserExist)
+	{
+		System.out.println ("\nPassword :\n");
+		pwd = sc.nextLine ();
+		if (pwd == "0")
+		{
+		b = false;
+
+		}
+		boolean verifypwd = a1.verifyPwd (pwd);
+		if (verifypwd)
+		{
+			System.out.println ("Login Successfull !");
+			while (b == true)
+			{
+				System.out.println(" Enter the option to perform : \n 1.CHECK BAL \n 2.TRANSFER \n 3.Exit");
+				option = sc.nextInt ();
+				sc.nextLine ();
+				if (option == 1)
+				{
+					float balamount = a1.checkbalance ();
+					System.out.println ("Account Balance : " + balamount);		
+				}
+				else if (option == 2)
+					{
+					System.out.println ("Enter the amount to transfer: ");
+					debitamount = sc.nextFloat();
+					//sc.nextLine ();
+					boolean checkBalForTrans = a1.checkForDebit (debitamount);
+					if (!checkBalForTrans)
+					{
+						System.out.println("Debit amount exceeds current balance. Please eneter valid amount !\n");
+					}
+					else
+					{
+						System.out.println ("Dear customer Rs." + debitamount +" has been debited from your account.");
+					}
+				}
+				else
+				{
+					//System.out.flush();  
+					System.out.println("Successfully logged out !");
+					b = false;
+				}
+			}
+		}
+		else
+		{
+			//System.out.flush();  
+			System.out.println("Password is incorrect !");
+		}
+			}
+			else
+			{
+				//System.out.flush();  
+				System.out.println("Username doesn't exist !  ");
+			}
+
+		}
+	}
+	else
+	{
+		flag = false;
+	}
+}while(flag == true);
+	}
+
+}
+
