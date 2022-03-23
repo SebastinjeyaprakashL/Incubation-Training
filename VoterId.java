7626de4116c685a64cc512b481eb2573cae62a4b
SebastinjeyaprakashL <79531993+SebastinjeyaprakashL@users.noreply.github.com>
2022-03-23 11:50:19 +0530

    Update VoterId.java

--- VoterId.java
+++ VoterId.java
@@ -14,7 +14,7 @@ class InvalidException extends Exception {
 public class VoterId {
 	boolean is_eligible, eligible_flag;
 	static int number_of_applicant = 0 ;
-	public static Map<Integer, List<Object>> voterList = new HashMap<>();
+	public static Map<Integer, List<String>> voterList = new HashMap<>();
 	
 	public VoterId () {
 		System.out.println("WELCOME");
@@ -32,64 +32,59 @@ public class VoterId {
 	}
 	
 	public void getApplicantDetails() throws InvalidException {
-		String applicant_name, applicant_gender;
-		int applicant_age;
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Enter the applicant's name :");
-		applicant_name = sc.nextLine();
-		System.out.println("Enter the applicant's age ( greater than 17 ):");
-		applicant_age = sc.nextInt();
-		sc.nextLine();
-		System.out.println("Enter the applicant's gender ( male / female / other ) :");
-		applicant_gender = sc.nextLine();
-		is_eligible = eligibilityCheck(applicant_age, applicant_gender);
-		if (is_eligible) {
-			registerVoterId (applicant_name, applicant_age, applicant_gender);
+		try {	
+			String applicant_name, applicant_gender;
+			int applicant_age;
+			Scanner sc = new Scanner (System.in);
+			System.out.println("Enter the applicant's name :");
+			applicant_name = sc.nextLine().toUpperCase();
+			System.out.println("Enter the applicant's age ( minimum age : 18 ):");
+			applicant_age = sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter the applicant's gender ( male / female / other ) :");
+			applicant_gender = sc.nextLine().toUpperCase();
+			is_eligible = eligibilityCheck(applicant_age, applicant_gender);
+			if (is_eligible) {
+				registerVoterId (applicant_name, applicant_age, applicant_gender);
+			}
+			else {
+				throw new InvalidException ("Invalid age or gender ! Please enter correct Age/Gender to apply");
+			}
 		}
-		else {
-			throw new InvalidException ("Invalid age or gender !");
+		catch (Exception e){
+			System.out.println(e);	
 		}
+		
 	}
 	
-	public void getVoterList (){
-		Iterator<Integer> itr = voterList.keySet().iterator();
-        while (itr.hasNext()) {
-            System.out.println(itr.next());
-        }
+	public void getVoterList (){	
+		voterList.forEach(
+                (key, value)->System.out.println("Applicant Id: " + key + " Applicant Details : " + value));		
 	}
 	
 	public void registerVoterId (String applicant_name, int applicant_age, String applicant_gender) {
 		number_of_applicant++;
-		List<Object> applicantDetails = new ArrayList <> ();
+		String ageStr = Integer.toString(applicant_age);
+		List<String> applicantDetails = new ArrayList <String> ();
 		applicantDetails.add(applicant_name);
-		applicantDetails.add(applicant_age);
+		applicantDetails.add(ageStr);
 		applicantDetails.add(applicant_gender);
 		voterList.put(number_of_applicant, applicantDetails);
 	}
 	
 	public boolean eligibilityCheck (int age, String gender) {
 		if (age > 17) {
-			String gender_to_upper = gender.toUpperCase();
-			Gender[] genders = Gender.values();
-			System.out.println (genders);
-			
-		}
-		return true;
-		
-		/*	for (Gender gender_values: genders) {
-				if (gender_values.equals(gender_to_upper)) {
+			Gender [] genders = Gender.values();
+			for(Gender val: genders) {
+				if (val.name().equalsIgnoreCase(gender)) {
 					eligible_flag = true;
 				}
-				else {
-					eligible_flag = false;
-				}
-			}
-			return eligible_flag ;
-			
+		    }
+			return eligible_flag;			
 		}
 		else {
 			return false;
-		} */
+		}	
 	}
 	
 	public static void main(String[] args) {
@@ -97,21 +92,21 @@ public class VoterId {
 			boolean option_flag = true;
 			int option;
 			Scanner sc = new Scanner (System.in);
-			VoterId applicant1 = new VoterId("ABC",19,"MALE");
+			VoterId applicant1 = new VoterId("User1",19,"MALE");
 			VoterId newApplicant = new VoterId();
-			
 			while(option_flag) {
 				System.out.println("Please enter any one of the option below :\n1.Apply Voter Id. \n2.View Voter List. \n3.Exit");
 				option = sc.nextInt();
 				sc.nextLine();
 				switch (option) {
-				case 1: newApplicant.getApplicantDetails();
-				break;
-				case 2: newApplicant.getVoterList();
-				break;
-				case 3: option_flag = false;
-				break;
-				default : System.out.println("No such option!");
+					case 1: newApplicant.getApplicantDetails();
+					break;
+					case 2: newApplicant.getVoterList();
+					break;
+					case 3: option_flag = false;
+							sc.close();
+					break;
+					default : System.out.println("No such option!");
 				}
 			}
 		}
@@ -119,6 +114,7 @@ public class VoterId {
 			e.printStackTrace();
 		}
 		finally {
+			System.gc();
 			System.out.println("Thank You!");
 		}
 		
